1248eb6b790d767d88d46c549252d371d6e1e0b8
Ed0139 <22610139@utgz.edu.mx>
2026-05-09 17:23:38 -0600

    Install the Prettier plugin for PHP

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,48 +6,48 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function index()
-    {
-        $cart = session()->get('cart', []);
-        return view('cart.index', compact('cart'));
-    }
+  public function index()
+  {
+    $cart = session()->get('cart', []);
+    return view('cart.index', compact('cart'));
+  }
 
-    public function add($id)
-{
+  public function add($id)
+  {
     $product = Product::findOrFail($id);
     $cart = session()->get('cart', []);
 
     if (isset($cart[$id])) {
-        $cart[$id]['quantity']++;
+      $cart[$id]['quantity']++;
     } else {
-        $cart[$id] = [
-            "name" => $product->name,
-            "price" => $product->price,
-            "quantity" => 1,
-            "image" => $product->image
-        ];
+      $cart[$id] = [
+        'name' => $product->name,
+        'price' => $product->price,
+        'quantity' => 1,
+        'image' => $product->image,
+      ];
     }
 
     session()->put('cart', $cart);
 
     return redirect()->back();
-}
+  }
 
-    public function remove($id)
-    {
-        $cart = session()->get('cart');
+  public function remove($id)
+  {
+    $cart = session()->get('cart');
 
-        if (isset($cart[$id])) {
-            unset($cart[$id]);
-            session()->put('cart', $cart);
-        }
-
-        return back();
+    if (isset($cart[$id])) {
+      unset($cart[$id]);
+      session()->put('cart', $cart);
     }
 
-    public function clear()
-    {
-        session()->forget('cart');
-        return back();
-    }
+    return back();
+  }
+
+  public function clear()
+  {
+    session()->forget('cart');
+    return back();
+  }
 }
